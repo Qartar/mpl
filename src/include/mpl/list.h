f20e388141bc7ef5442607241284ac2fd4968ff9
Carter Bray <carter.bray@intel.com>
2015-08-23 12:20:08 -0700

    Extend implementation

--- src/include/mpl/list.h
+++ src/include/mpl/list.h
@@ -29,16 +29,44 @@ struct fn_list<_Tx> {
     using type = cons<_Tx, nil>;
 };
 
+///////////////////////////////////////////////////////////////////////////////
+//! Implementation of `extend` metafunction
+template<typename... _Targs> struct fn_extend;
+
+template<typename _Tx, typename _Ty, typename... _Targs>
+struct fn_extend<_Tx, _Ty, _Targs...> {
+    using type = typename fn_extend<typename fn_extend<_Tx, _Ty>::type, _Targs...>::type;
+};
+
+template<typename _Tx, typename _Ty, typename _Tz>
+struct fn_extend<cons<_Tx, _Ty>, _Tz> {
+    using type = cons<_Tx, typename fn_extend<_Ty, _Tz>::type>;
+};
+
+template<typename _Tx, typename _Ty>
+struct fn_extend<cons<_Tx, nil>, _Ty> {
+    using type = cons<_Tx, _Ty>;
+};
+
 } // namespace detail
 
 ///////////////////////////////////////////////////////////////////////////////
 /**
  * list
  *
- * Fundamental metafunction for constructing an s-expression list from a
- * variadic list of type arguments.
+ * Fundamental metafunction for constructing an s-expression list metatype from
+ * a variadic list of type arguments.
  */
 template<typename... _Targs> using list = typename detail::fn_list<_Targs...>::type;
+
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * extend
+ *
+ * Metafunction for concatenating two or more s-expression list metatypes into
+ * a single s-expression list metatype.
+ */
+template<typename... _Targs> using extend = typename detail::fn_extend<_Targs...>::type;
 
 } // namespace mpl
 
--- src/test/list.cpp
+++ src/test/list.cpp
@@ -2,6 +2,8 @@
 
 #include <type_traits>
 
+namespace {
+
 namespace A {
 
 using _A = mpl::list<int>;
@@ -40,3 +42,81 @@ static_assert(std::is_same<float, _D>::value, "");
 static_assert(std::is_same<mpl::nil, _E>::value, "");
 
 } // namespace C
+
+namespace D {
+
+using _A = mpl::list<int>;
+using _B = mpl::list<bool>;
+using _C = mpl::extend<_A, _B>;
+using _D = mpl::car<_C>;
+using _E = mpl::car<mpl::cdr<_C>>;
+using _F = mpl::cdr<mpl::cdr<_C>>;
+
+static_assert(std::is_same<int, _D>::value, "");
+static_assert(std::is_same<bool, _E>::value, "");
+static_assert(std::is_same<mpl::nil, _F>::value, "");
+
+} // namespace D
+
+namespace E {
+
+using _A = mpl::list<int>;
+using _B = mpl::list<bool>;
+using _C = mpl::list<float>;
+using _D = mpl::extend<_A, _B, _C>;
+using _E = mpl::car<_D>;
+using _F = mpl::car<mpl::cdr<_D>>;
+using _G = mpl::car<mpl::cdr<mpl::cdr<_D>>>;
+using _H = mpl::cdr<mpl::cdr<mpl::cdr<_D>>>;
+
+static_assert(std::is_same<int, _E>::value, "");
+static_assert(std::is_same<bool, _F>::value, "");
+static_assert(std::is_same<float, _G>::value, "");
+static_assert(std::is_same<mpl::nil, _H>::value, "");
+
+} // namespace E
+
+namespace F {
+
+using _A = mpl::list<int, bool>;
+using _B = mpl::list<float, double>;
+using _C = mpl::extend<_A, _B>;
+using _D = mpl::car<_C>;
+using _E = mpl::car<mpl::cdr<_C>>;
+using _F = mpl::car<mpl::cdr<mpl::cdr<_C>>>;
+using _G = mpl::car<mpl::cdr<mpl::cdr<mpl::cdr<_C>>>>;
+using _H = mpl::cdr<mpl::cdr<mpl::cdr<mpl::cdr<_C>>>>;
+
+static_assert(std::is_same<int, _D>::value, "");
+static_assert(std::is_same<bool, _E>::value, "");
+static_assert(std::is_same<float, _F>::value, "");
+static_assert(std::is_same<double, _G>::value, "");
+static_assert(std::is_same<mpl::nil, _H>::value, "");
+
+} // namespace F
+
+namespace G {
+
+using _A = mpl::list<int, bool>;
+using _B = mpl::list<float, double>;
+using _C = mpl::list<wchar_t, unsigned>;
+using _D = mpl::extend<_A, _B, _C>;
+using _E = mpl::car<_D>;
+using _F = mpl::car<mpl::cdr<_D>>;
+using _G = mpl::car<mpl::cdr<mpl::cdr<_D>>>;
+using _H = mpl::car<mpl::cdr<mpl::cdr<mpl::cdr<_D>>>>;
+using _I = mpl::car<mpl::cdr<mpl::cdr<mpl::cdr<mpl::cdr<_D>>>>>;
+using _J = mpl::car<mpl::cdr<mpl::cdr<mpl::cdr<mpl::cdr<mpl::cdr<_D>>>>>>;
+using _K = mpl::cdr<mpl::cdr<mpl::cdr<mpl::cdr<mpl::cdr<mpl::cdr<_D>>>>>>;
+
+static_assert(std::is_same<int, _E>::value, "");
+static_assert(std::is_same<bool, _F>::value, "");
+static_assert(std::is_same<float, _G>::value, "");
+static_assert(std::is_same<double, _H>::value, "");
+static_assert(std::is_same<wchar_t, _I>::value, "");
+static_assert(std::is_same<unsigned, _J>::value, "");
+static_assert(std::is_same<mpl::nil, _K>::value, "");
+
+} // namespace G
+
+} // anonymous namespace
