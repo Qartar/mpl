22609a6a7d4ad3b5f46308f37b3ffe5376027e0c
Carter Bray <carter.bray@intel.com>
2015-08-25 15:22:20 -0700

    Add tests for STL interop

--- src/test/logical.cpp
+++ src/test/logical.cpp
@@ -49,6 +49,36 @@ ASSERT_SAME(_C, mpl::false_type);
 ASSERT_SAME(_D, mpl::false_type);
 
 } // namespace C
+
+//-----------------------------------------------------------------------------
+namespace D {
+
+using _A = mpl::and<std::true_type, std::true_type>;
+using _B = mpl::and<std::true_type, std::false_type>;
+using _C = mpl::and<std::false_type, std::true_type>;
+using _D = mpl::and<std::false_type, std::false_type>;
+
+ASSERT_SAME(_A, mpl::true_type);
+ASSERT_SAME(_B, mpl::false_type);
+ASSERT_SAME(_C, mpl::false_type);
+ASSERT_SAME(_D, mpl::false_type);
+
+} // namespace D
+
+//-----------------------------------------------------------------------------
+namespace E {
+
+using _A = mpl::and<std::true_type, mpl::true_type>;
+using _B = mpl::and<std::true_type, mpl::false_type>;
+using _C = mpl::and<std::false_type, mpl::true_type>;
+using _D = mpl::and<std::false_type, mpl::false_type>;
+
+ASSERT_SAME(_A, mpl::true_type);
+ASSERT_SAME(_B, mpl::false_type);
+ASSERT_SAME(_C, mpl::false_type);
+ASSERT_SAME(_D, mpl::false_type);
+
+} // namespace E
 } // namespace and
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -98,4 +128,34 @@ ASSERT_SAME(_C, mpl::true_type);
 ASSERT_SAME(_D, mpl::false_type);
 
 } // namespace C
+
+//-----------------------------------------------------------------------------
+namespace D {
+
+using _A = mpl::or<std::true_type, std::true_type>;
+using _B = mpl::or<std::true_type, std::false_type>;
+using _C = mpl::or<std::false_type, std::true_type>;
+using _D = mpl::or<std::false_type, std::false_type>;
+
+ASSERT_SAME(_A, mpl::true_type);
+ASSERT_SAME(_B, mpl::true_type);
+ASSERT_SAME(_C, mpl::true_type);
+ASSERT_SAME(_D, mpl::false_type);
+
+} // namespace D
+
+//-----------------------------------------------------------------------------
+namespace E {
+
+using _A = mpl::or<std::true_type, mpl::true_type>;
+using _B = mpl::or<std::true_type, mpl::false_type>;
+using _C = mpl::or<std::false_type, mpl::true_type>;
+using _D = mpl::or<std::false_type, mpl::false_type>;
+
+ASSERT_SAME(_A, mpl::true_type);
+ASSERT_SAME(_B, mpl::true_type);
+ASSERT_SAME(_C, mpl::true_type);
+ASSERT_SAME(_D, mpl::false_type);
+
+} // namespace E
 } // namespace or
